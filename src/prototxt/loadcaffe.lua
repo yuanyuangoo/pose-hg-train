1f6e043fff36d119e4b50478c3ece52d3321351e
yuanyuangoo <yuanyuangoo@hotmail.com>
2017-06-17 13:40:15 +1200

    0617

--- src/prototxt/loadcaffe.lua
+++ src/prototxt/loadcaffe.lua
@@ -11,12 +11,21 @@ require 'cunn'
 require 'cutorch'
 require 'cudnn'
 require('transtorch')
+require("csvigo")
 
-caffeNet = transTorch.loadCaffe('./all.prototxt')
+--caffeNet = transTorch.loadCaffe('./prototxt/all.prototxt')
 --transTorch.toCaffe(l, caffeNet, "cnv1_")
 
-net = torch.load('umich-stacked-hourglass.t7')
-for i = 1, 280 do
-    print("layer", tostring(i))
-    print(net:get(i))
+--net = torch.load('./prototxt/umich-stacked-hourglass.t7')
+
+layers = csvigo.load({path = "./prototxt/layers.csv", mode = "large"})
+
+for i, v in ipairs(layers) do
+    if v[3] == "" then
+        transTorch.toCaffe(net:get(tonumber(v[2])), caffeNet, v[1])
+    elseif v[3] == "2" then
+        transTorch.toCaffe(net:get(tonumber(v[2])):get(2), caffeNet, v[1])
+    else
+        transTorch.toCaffe(net:get(tonumber(v[2])):get(1):get(tonumber(v[4])):get(tonumber(v[5])), caffeNet, v[1])
+    end
 end
