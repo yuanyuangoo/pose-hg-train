299e3923a916c651d42bf60c9a9bbf1f5a0fb4e8
yuanyuangoo <yuanyuangoo@hotmail.com>
2017-06-21 10:42:24 +1200

    0621

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ int main(int argc, char** argv)
 {
 	char* hg_proto = "D:/happynear/caffe-windows-ms/windows/pose-hg-train/src/prototxt/all.prototxt";
 	Phase phase=TRAIN;
-	Caffe::set_mode(Caffe::CPU);
+	Caffe::set_mode(Caffe::GPU);
+	Caffe::SetDevice(0);
 	boost::shared_ptr<Net<float>>net(new caffe::Net<float>(hg_proto, phase));
 	return 1;
 }
